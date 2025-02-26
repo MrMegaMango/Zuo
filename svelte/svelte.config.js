80ff2c978891f323d9a827a579042b56c758084e
Zuo <zuo@samaya.ai>
2025-02-26 10:28:30 +0000

    install on vercel build

--- svelte/svelte.config.js
+++ svelte/svelte.config.js
@@ -3,31 +3,26 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: 'index.html'
-		}),
-		paths: {
-			base: '/app'
-		},
-		prerender: {
-			handleHttpError: ({ path, referrer, message }) => {
-				// Ignore specific errors related to base path
-				if (message.includes('does not begin with `base`')) {
-					return;
-				}
-				// Throw other errors
-				throw new Error(message);
-			}
-		}
-	}
+    kit: {
+        adapter: adapter({
+            fallback: 'index.html'
+        }),
+        paths: {
+            base: '/app'
+        },
+        prerender: {
+            handleHttpError: ({ path, referrer, message }) => {
+                // Ignore specific errors related to base path
+                if (message.includes('does not begin with `base`')) {
+                    return;
+                }
+                // Throw other errors
+                throw new Error(message);
+            }
+        }
+    }
 };
 
 export default config;
